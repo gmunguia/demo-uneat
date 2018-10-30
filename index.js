62585b09639c06d5e2c626c71e63aa2bcdf80301
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2018-10-30 12:44:36 +0100

    add body-parser, echo route

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const server = express();
+server.use(bodyParser.json());
 
 server.get("/hello", (req, res) => {
   res.send("Hello World");
+});
+
+server.post("/echo", (req, res) => {
+  res.json(req.body);
 });
 
 server.listen(3000);
