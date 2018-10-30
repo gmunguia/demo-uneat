1d94f003f12509af2d3badf1d90ccc81f4a91542
Gerardo Munguia <gerardomunguiahoyo@gmail.com>
2018-10-30 19:56:43 +0100

    add game of life routes and logic

--- index.js
+++ index.js
@@ -12,7 +12,45 @@ server.post("/echo", (req, res) => {
   res.json(req.body);
 });
 
-let life = [];
+let life = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0]
+];
+
+const getCell = (life, x, y) => life[y] && life[y][x];
+
+const getNeighbors = (life, x, y) => [
+  getCell(life, x - 1, y - 1),
+  getCell(life, x - 1, y),
+  getCell(life, x - 1, y + 1),
+  getCell(life, x, y - 1),
+  getCell(life, x, y + 1),
+  getCell(life, x + 1, y - 1),
+  getCell(life, x + 1, y),
+  getCell(life, x + 1, y + 1)
+];
+
+const countAliveNeighbors = (life, x, y) => {
+  const add = (a, b) => a + b;
+
+  return getNeighbors(life, x, y)
+    .filter(neighbor => neighbor != undefined)
+    .reduce(add, 0);
+}
+
+const nextGeneration = life =>
+  life.map((row, y) => row.map((cell, x) => {
+    if (cell === 1 && [2, 3].includes(countAliveNeighbors(life, x, y))) {
+      return 1;
+    }
+    if (cell === 0 && countAliveNeighbors(life, x, y) === 3) {
+      return 1;
+    }
+    return 0;
+  }));
 
 server.get("/life", (req, res) => {
   res.json(life);
@@ -20,6 +58,11 @@ server.get("/life", (req, res) => {
 
 server.post("/life", (req, res) => {
   life = req.body;
+  res.json(life);
+});
+
+server.post("/life/next", (req, res) => {
+  life = nextGeneration(life);
   res.json(life);
 });
 
